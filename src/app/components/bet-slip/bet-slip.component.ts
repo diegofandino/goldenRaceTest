af2a171d028f6ce78260b524a5ac2bc9b8e8c796
DiegoFandino <diegofandino.u@gmail.com>
2022-01-17 17:16:14 -0500

    The indices are replaced when selecting the ballots

--- src/app/components/bet-slip/bet-slip.component.ts
+++ src/app/components/bet-slip/bet-slip.component.ts
@@ -12,16 +12,52 @@ export class BetSlipComponent implements OnInit {
   arrayNumberChosen: number[] = [];
   arrayNumberBalls: number[] = [];
   priceControl: number = 0;
+  totalAmount: number = 0;
   constructor(private ballService :BallServicesService ) { }
 
   ngOnInit(): void {
     
+    //indexBalls
+    let index = 0;
+
     //Number of balls
-    this.arrayNumberBalls = Array.from(Array(this.ballService.numberBalls).keys())
+    this.arrayNumberChosen =  Array.from({length: this.ballService.numberBalls}, (_, i) => 0);
+
     //Balls
     this.ballService.chosenNumbers.subscribe((value) => {
-        this.arrayNumberChosen = value;
+      if(value.length <= 0){
+        this.arrayNumberChosen =  Array.from({length: this.ballService.numberBalls}, (_, i) => 0);
+        index = 0;
+        return;
+      }
+      this.arrayNumberChosen[index] = value[value.length - 1];
+      index++;
    });
+  }
+
+  validateMinAmount(){
+    if(this.priceControl <= 0){
+      alert('Minimum bet is 5€');
+      return;
+    }
+
+    this.totalAmount = this.priceControl * 5;
+
+  }
+
+  placeBet(){
+    
+    for( var i = 0; i < this.arrayNumberChosen.length; i++){ 
+      
+      if ( this.arrayNumberChosen[i] === 0) { 
+        
+        this.arrayNumberChosen.splice(i, 1);
+        i--;
+      }
+      
+    }
+    
+    console.log(this.arrayNumberChosen);
   }
 
 }
